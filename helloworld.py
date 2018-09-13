a243c6e68676abecf83654fbf79b57a36c6033cb
Shimraz <43060427+Shimraz@users.noreply.github.com>
2018-09-13 12:43:16 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+class String: 
+
+	#Dunder method to initiate object
+	def __init__(self,string):
+		self.string=string
+
+	#print out string object
+	def __repr__(self):   
+		return 'Object: {}'.format(self.string)
+
+#driver code
+if __name__=='__main__'
+	#object creation
+	string1=String("Hello World Program using Dunders")
+	print(string1)
+
